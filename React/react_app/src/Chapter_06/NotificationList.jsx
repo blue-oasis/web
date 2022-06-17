f89f11942e0c618ac76957495e0ad381f27fe1da
JiWon <radiohead@knou.ac.kr>
2022-06-17 12:58:05 +0900

    콘솔 알림 id
    메시지 id 추가로 구별할 수 있게 함

--- React/react_app/src/Chapter_06/NotificationList.jsx
+++ React/react_app/src/Chapter_06/NotificationList.jsx
@@ -3,12 +3,15 @@ import Notification from "./Notification";
 
 const reservedNotifications = [
     {
+        id: 1,
         message: "안녕하세요, 일정 안내드립니다."
     },
     {
+        id: 2,
         message: "점심식사 시간입니다.",
     },
     {
+        id: 3,
         message: "이제 곧 미팅이 시작됩니다.",
     },
 ];
@@ -43,7 +46,7 @@ class NotificationList extends React.Component {
         return (
             <div>
                 {this.state.notifications.map((notification) => {
-                    return <Notification message={notification.message} />;
+                    return <Notification key={notification.id} id={notification.id} message={notification.message} />;
                 })}
             </div>
         );
